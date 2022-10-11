1f94dedd562ed847d58db9fdd827ba46d9070feb
Tim <$GIT_EMAIL>
2022-10-11 08:17:43 +0200

    Create profile models and associate them with User

--- profiles/models.py
+++ profiles/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Legal(models.Model):
+    user = models.OneToOneField(User, on_delete=models.PROTECT)
+    name = models.CharField(max_length=200)
+    legal_representative_first_name = models.CharField(max_length=200)
+    legal_representative_last_name = models.CharField(max_length=200)
+    phone_number = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.name
+
+class Natural(models.Model):
+    user = models.OneToOneField(User, on_delete=models.PROTECT)
+    first_name = models.CharField(max_length=200)
+    last_name = models.CharField(max_length=200)
+    phone_number = models.CharField(max_length=20)
+    place_of_birth = models.CharField(max_length=200)
+
+    def __str__(self):
+        return f'{self.first_name} {self.last_name}'
