f7e960c27e514eb5e65aa19383f783385e78dc33
Tim <$GIT_EMAIL>
2022-10-12 14:12:48 +0200

    Give precedence over custom views over django auth

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,6 +3,5 @@ from django.urls import include, path
 from .views import profiles
 
 urlpatterns = [
-    path('', include('django.contrib.auth.urls')),
     path('profile/', profiles.show, name='profile_show')
 ]
