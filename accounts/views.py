f28510597742eda65492e0413f8e518a506e3932
Tim <$GIT_EMAIL>
2022-10-13 17:12:13 +0200

    Declare method as private

--- accounts/views.py
+++ accounts/views.py
@@ -15,13 +15,13 @@ def signup(request):
         form = SignUpForm(request.POST)
         if form.is_valid():
             user = form.save()
-            associate_user_and_profile(user, form)
+            _associate_user_and_profile(user, form)
             return redirect('login')
     else:
         form = SignUpForm()
     return render(request, 'registration/signup.html', { 'form': form })
 
-def associate_user_and_profile(user, form):
+def _associate_user_and_profile(user, form):
     profile_type = form.cleaned_data['profile_type']
     if profile_type == 'Legal':
         Legal(user=user).save()
