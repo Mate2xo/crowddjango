ac20c83d3c002461792dad56866f4091de56dcbe
Tim <$GIT_EMAIL>
2022-10-19 18:41:37 +0200

    Add I18n to models and sign-up form

--- accounts/models.py
+++ accounts/models.py
@@ -1,10 +1,15 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.utils.translation import gettext_lazy as _
 from polymorphic.models import PolymorphicModel
 
 class Profile(PolymorphicModel):
     user = models.OneToOneField(User, on_delete=models.PROTECT)
     phone_number = models.CharField(max_length=20)
+
+    class Meta:
+        verbose_name = _('profile')
+        verbose_name_plural = _('profiles')
 
 class Legal(Profile):
     name = models.CharField(max_length=200)
@@ -14,8 +19,16 @@ class Legal(Profile):
     def __str__(self):
         return self.name or f'{self.user.first_name} {self.user.last_name}'
 
+    class Meta:
+        verbose_name = _('legal entity')
+        verbose_name_plural = _('legal entities')
+
 class Natural(Profile):
     place_of_birth = models.CharField(max_length=200)
 
     def __str__(self):
         return f'{self.user.first_name} {self.user.last_name}'
+
+    class Meta:
+        verbose_name = _('natural person')
+        verbose_name_plural = _('natural persons')
--- investments/models.py
+++ investments/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 from accounts.models import Profile
 
 class Fund(models.Model):
@@ -6,6 +7,10 @@ class Fund(models.Model):
 
     def __str__(self):
         return self.name
+
+    class Meta:
+        verbose_name = _('fund')
+        verbose_name_plural = _('funds')
 
 class Subscription(models.Model):
     amount = models.PositiveIntegerField()
@@ -16,3 +21,7 @@ class Subscription(models.Model):
         full_name = f'{self.profile.user.first_name} {self.profile.user.last_name}'.strip()
 
         return full_name or self.profile.user.username
+
+    class Meta:
+        verbose_name = _('subscription')
+        verbose_name_plural = _('subscriptions')
