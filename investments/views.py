c582f708ed03453729fc8d27896a22de7012f8b5
Tim <$GIT_EMAIL>
2022-10-20 14:21:26 +0200

    Scope Subscriptions to current user

--- investments/views.py
+++ investments/views.py
@@ -5,5 +5,8 @@ from .models import Subscription
 class SubscriptionsList(LoginRequiredMixin, generic.ListView):
     model = Subscription
 
+    def get_queryset(self):
+        return self.request.user.profile.subscription_set.all()
+
 class SubscriptionDetail(LoginRequiredMixin, generic.DetailView):
     model = Subscription
