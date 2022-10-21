64f5c22adfd00e428741b7e14b2cc3d4a8dd28ca
Tim <$GIT_EMAIL>
2022-10-21 12:43:43 +0200

    Add basic tests for investments app

--- investments/tests.py
+++ investments/tests.py
@@ -1,3 +1,65 @@
+from django.contrib.auth.models import User
 from django.test import TestCase
+from django.urls import reverse
+from django.utils.translation import gettext as _
 
-# Create your tests here.
+from .models import Fund, Subscription
+from accounts.models import Legal, Natural
+
+def create_subscription(username='patate', fundname='friteuse', companyname='Hocouteau no Ken'):
+    user = User.objects.create_user(username=username, password='passpass')
+    profile = Legal.objects.create(user=user, phone_number='007', name=companyname)
+    fund = Fund.objects.create(name=fundname)
+    return Subscription.objects.create(amount=1234, profile=profile, fund=fund)
+
+class SubscriptionModelTests(TestCase):
+    def test_subscription_display_is_the_legal_profile_name(self):
+        subscription = create_subscription()
+
+        self.assertEqual(repr(subscription), f'<Subscription: {subscription.profile.name}>')
+
+class SubscriptionViewTests(TestCase):
+    def test_index_requires_login(self):
+        response = self.client.get(reverse('investments:dashboard'))
+
+        self.assertIn('accounts/login', response.url)
+
+    def test_index_only_shows_the_current_user_subscriptions(self):
+        subscription1 = create_subscription()
+        subscription2 = create_subscription(username='frite', fundname='au four')
+        self.client.login(username=subscription1.profile.user, password='passpass')
+
+        response = self.client.get(reverse('investments:dashboard'))
+
+        self.assertQuerysetEqual(response.context['object_list'], [subscription1])
+
+    def test_index_shows_message_when_there_are_no_subscriptions(self):
+        user = User.objects.create_user(username='newbe', password='passpass')
+        Legal.objects.create(user=user, phone_number='007', name='omae ha mou shindeiru')
+        self.client.login(username=user.username, password='passpass')
+
+        response = self.client.get(reverse('investments:dashboard'))
+
+        self.assertContains(response, _('You have not subscribed to any fund'))
+
+    def test_show_requires_login(self):
+        response = self.client.get(reverse('investments:subscription_detail', args=[999]))
+
+        self.assertIn('accounts/login', response.url)
+
+    def test_show_is_200_for_current_user_subscription(self):
+        subscription = create_subscription()
+        self.client.login(username=subscription.profile.user, password='passpass')
+
+        response = self.client.get(reverse('investments:subscription_detail', args=[subscription.id]))
+
+        self.assertEqual(response.status_code, 200)
+
+    def test_show_is_404_for_other_user_subscription(self):
+        other_user_subscription = create_subscription() # for another user
+        User.objects.create_user(username='current', password='passpass')
+        self.client.login(username='current', password='passpass')
+
+        response = self.client.get(reverse('investments:subscription_detail', args=[other_user_subscription.id]))
+
+        self.assertEqual(response.status_code, 404)
--- investments/views.py
+++ investments/views.py
@@ -1,6 +1,10 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.http import Http404
+from django.shortcuts import get_object_or_404
 from django.views import generic
+
 from .models import Subscription
+from accounts.models import Profile
 
 class SubscriptionsList(LoginRequiredMixin, generic.ListView):
     model = Subscription
@@ -10,3 +14,10 @@ class SubscriptionsList(LoginRequiredMixin, generic.ListView):
 
 class SubscriptionDetail(LoginRequiredMixin, generic.DetailView):
     model = Subscription
+
+    def get_object(self):
+        try:
+            scoped_manager = self.request.user.profile.subscription_set
+        except Profile.DoesNotExist:
+            raise Http404()
+        return get_object_or_404(scoped_manager)
