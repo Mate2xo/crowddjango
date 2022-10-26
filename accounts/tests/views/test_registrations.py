fd2e59f3af81f57df5779b102a232f2d4223d0ab
Tim <$GIT_EMAIL>
2022-10-26 17:34:41 +0200

    Replace View private logic to its own service

--- accounts/tests/views/test_registrations.py
+++ accounts/tests/views/test_registrations.py
@@ -1,8 +1,6 @@
-from django.contrib.auth.models import User
 from django.test import TestCase
 from django.urls import reverse
-
-from accounts.models import Profile
+from unittest.mock import patch
 
 class SignUpGet(TestCase):
     def test_http_ok_status(self):
@@ -11,16 +9,32 @@ class SignUpGet(TestCase):
         self.assertEqual(response.status_code, 200)
 
 class SignUpPost(TestCase):
-    def test_with_valid_arguments_associates_user_with_profile(self):
-        params = {
+    def setUp(self):
+        self.valid_params = {
             'username': 'polo_et_pan',
             'first_name': 'polo',
             'last_name': 'pan',
             'password1': 'P@assword1',
             'password2': 'P@assword1',
-            'profile_type': 'Natural'
+            'profile_type': 'Legal'
         }
 
-        self.client.post('/accounts/signup/', params)
+    @patch('accounts.services.UserRegistration.perform', autospec=True)
+    def test_with_successful_user_registration_redirects_to_login(self, mocked_method):
+        mocked_method.return_value = True
 
-        self.assertIsInstance(User.objects.last().profile, Profile)
+        response = self.client.post('/accounts/signup/', self.valid_params)
+
+        assert mocked_method.called
+        self.assertRedirects(response, reverse('login'))
+
+    @patch('accounts.services.UserRegistration.perform', autospec=True)
+    def test_with_failed_user_registration_redirects_to_signup(self, mocked_method):
+        mocked_method.return_value = False
+        invalid_params = self.valid_params
+        invalid_params['profile_type'] = ''
+
+        response = self.client.post('/accounts/signup/', invalid_params)
+
+        assert mocked_method.called
+        self.assertTemplateUsed(response, 'registration/signup.html')
--- accounts/views.py
+++ accounts/views.py
@@ -1,10 +1,9 @@
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
 from django.shortcuts import redirect, render
-from django.urls import reverse_lazy
-from django.views import generic
-from .models import Legal, Natural
+
 from .forms import SignUpForm
+from .services import UserRegistration
 
 @login_required()
 def profile(request):
@@ -13,17 +12,8 @@ def profile(request):
 def signup(request):
     if request.method == 'POST':
         form = SignUpForm(request.POST)
-        if form.is_valid():
-            user = form.save()
-            _associate_user_and_profile(user, form)
+        if UserRegistration.perform(form):
             return redirect('login')
     else:
         form = SignUpForm()
     return render(request, 'registration/signup.html', { 'form': form })
-
-def _associate_user_and_profile(user, form):
-    profile_type = form.cleaned_data['profile_type']
-    if profile_type == 'Legal':
-        Legal(user=user).save()
-    elif profile_type == 'Natural':
-        Natural(user=user).save()
