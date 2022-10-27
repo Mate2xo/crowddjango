bab3be611a27e4111f747799886f978cfadcad33
Tim <$GIT_EMAIL>
2022-10-27 17:48:43 +0200

    Install bootstrap, create navbar

--- mysite/settings.py
+++ mysite/settings.py
@@ -39,6 +39,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'accounts.apps.AccountsConfig',
+    'bootstrap5',
     'investments.apps.InvestmentsConfig',
     'polymorphic',
     'django.contrib.admin',
